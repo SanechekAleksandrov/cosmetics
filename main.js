fbc2e2abe9e1614b5933ad7b0a8f7c3415825f59
Sanechek Aleksandrov <sanechek-1994@mail.ru>
2025-02-12 18:18:05 +0100

    Added production section

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ const swiper = new Swiper('.swiper', {
     crossFade: true 
   }
 });
+
+const swiperTwo = new Swiper('.swiperTwo', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  slidesPerView: 3, // Показывать 3 слайда одновременно
+    spaceBetween: 20, // Отступ между слайдами (по желанию)
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+})
